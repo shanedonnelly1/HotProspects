7af2bf205126bc19ee725959a92e8756780970bc
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-12-15 09:00:21 +0000

    Initial Commit

---
+++ Shared/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  Shared
+//
+//  Created by Shane on 15/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
